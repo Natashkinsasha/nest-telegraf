09e025d56f398d62f5dedf8bf6993e8af4b8d96c
Alex <natashkinsasha@whitesharx.com>
2019-08-29 11:52:37 +0300

    v 1.0.4

--- src/TelegrafModule.ts
+++ src/TelegrafModule.ts
@@ -1,12 +1,12 @@
 import Telegraf, {ContextMessageUpdate, TelegrafOptions} from 'telegraf';
 import {DynamicModule, ForwardReference, Module, Type} from "@nestjs/common";
 import {ModuleRef} from "@nestjs/core";
-import {handlers, properties} from "./decorator/BotController";
+import {controllers, handlers, properties} from "./decorator/BotController";
 
 @Module({})
 export default class TelegrafModule {
 
-    public static forRootAsync({useFactory, inject, imports}: { useFactory: (...args: any[]) => Promise<{ token: string, options?: TelegrafOptions, }> | { token: string, options?: TelegrafOptions, }, inject?: Array<Type<any> | string | symbol>, imports?: Array<Type<any> | DynamicModule | Promise<DynamicModule> | ForwardReference>; }): DynamicModule {
+    public static forRootAsync({ useFactory, inject, imports }: { useFactory: (...args: any[]) => Promise<{ token: string, options?: TelegrafOptions, }> | { token: string, options?: TelegrafOptions, }, inject?: Array<Type<any> | string | symbol>, imports?: Array<Type<any> | DynamicModule | Promise<DynamicModule> | ForwardReference>; }): DynamicModule {
         const providers = [
             {
                 provide: Telegraf,
@@ -14,19 +14,19 @@ export default class TelegrafModule {
                     return Promise
                         .resolve()
                         .then(() => useFactory(...inject))
-                        .then(({token, options}) => {
+                        .then(({ token, options }) => {
                             return this.init(ref, token, options);
-                        })
+                        });
                 },
-                inject: [ModuleRef, ...(inject || [])]
-            }
+                inject: [ModuleRef, ...(inject || [])],
+            },
         ];
         return {
             imports,
             module: TelegrafModule,
             providers: providers,
             exports: providers,
-        }
+        };
     }
 
 
@@ -37,48 +37,55 @@ export default class TelegrafModule {
                 useFactory: async (ref: ModuleRef) => {
                     return this.init(ref, token, options);
                 },
-                inject: [ModuleRef]
-            }
+                inject: [ModuleRef],
+            },
         ];
         return {
             module: TelegrafModule,
             providers: providers,
             exports: providers,
-        }
+        };
     }
 
     private static async init(ref: ModuleRef, token: string, options?: TelegrafOptions) {
         const telegraf = new Telegraf(token, options);
-        handlers.forEach((handler) => {
-            const object: Object | undefined = ref.get(handler.class, {strict: false});
-            if (!object) {
-                return;
-            }
-            const func = Object.getOwnPropertyDescriptor(Object.getPrototypeOf(object), handler.method);
-            if (!func) {
-                return;
-            }
-            if (handler.type === 'hears') {
-                const triggers = handler.property[0];
-                return telegraf.hears(triggers, (ctx: ContextMessageUpdate, next?: () => any) => {
-                    const ctxProperty = properties.find((property) => {
-                        return property.target === Object.getPrototypeOf(object) && property.propertyKey === func.value.name && property.type === 'context';
-                    });
-                    const nextProperty = properties.find((property) => {
-                        return property.target === Object.getPrototypeOf(object) && property.propertyKey === func.value.name && property.type === 'next';
-                    });
-                    const args: any[] = [];
-                    if (ctxProperty) {
-                        args[ctxProperty.parameterIndex] = ctx;
-                    }
-                    if (nextProperty) {
-                        args[nextProperty.parameterIndex] = next;
-                    }
-                    return func.value.apply(object, args)
+        handlers
+            .filter((handler) => {
+                return controllers.find((controller) => {
+                    return controller.class === handler.class;
                 });
-            }
-            return;
-        });
+            })
+            .forEach((handler) => {
+                const object: Object | undefined = ref.get(handler.class, { strict: false });
+                if (!object) {
+                    return;
+                }
+                const func = Object.getOwnPropertyDescriptor(Object.getPrototypeOf(object), handler.method);
+                if (!func) {
+                    return;
+                }
+                if (handler.type === 'hears') {
+                    const triggers = handler.property[0];
+                    return telegraf.hears(triggers, (ctx: ContextMessageUpdate, next: () => any) => {
+                        const ctxProperty = properties.find((property) => {
+                            return property.target === Object.getPrototypeOf(object) && property.propertyKey === func.value.name && property.type === 'context';
+                        });
+                        const nextProperty = properties.find((property) => {
+                            return property.target === Object.getPrototypeOf(object) && property.propertyKey === func.value.name && property.type === 'next';
+                        });
+                        const args: any[] = [];
+                        if (ctxProperty) {
+                            args[ctxProperty.parameterIndex] = ctx;
+                        }
+                        if (nextProperty) {
+                            args[nextProperty.parameterIndex] = next;
+                        }
+                        const object2: Object | undefined = ref.get(handler.class, { strict: false });
+                        return func.value.apply(object2, args);
+                    });
+                }
+                return;
+            });
         return telegraf;
     }
 }
--- src/decorator/BotController.ts
+++ src/decorator/BotController.ts
@@ -1,16 +1,19 @@
-import {Injectable, InjectableOptions, Type} from "@nestjs/common";
+import { Injectable, InjectableOptions, Type } from '@nestjs/common';
 
 type HandlerType = 'hears' | 'start' | 'on' | 'help';
 
 type PropertyType = 'context' | 'next'
 
-const handlers: Array<{class: Type<any>, method: string, type: HandlerType, property: any[]}> = [];
+const handlers: Array<{ class: Type<any>, method: string, type: HandlerType, property: any[] }> = [];
 
-const properties: Array<{target: Object, propertyKey: string | symbol, parameterIndex: number, type: PropertyType}> = [];
+const properties: Array<{ target: Object, propertyKey: string | symbol, parameterIndex: number, type: PropertyType }> = [];
 
-export {handlers, properties} ;
+const controllers: Array<{ class: Type<any> }> = [];
+
+export { handlers, properties, controllers } ;
 
 export default (options?: InjectableOptions) => {
-    return (constructor: Function) => {
-    }
+    return (constructor: any) => {
+        controllers.push({ class: constructor });
+    };
 }
